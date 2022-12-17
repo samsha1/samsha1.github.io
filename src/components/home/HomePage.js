435251d8634237551af85e81aebfebdbcc56682b
samrat <samratshakya5@gmail.com>
2022-12-17 18:13:28 +0545

    cv  link

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './styles.scss';
+import Button from '@material-ui/core/Button';
 import SocialIcons from './SocialIcons';
-import PlayNine from './PlayNine';
-
+import CloudDownloadOutlinedIcon from '@material-ui/icons/CloudDownloadOutlined';
 class HomePage extends React.Component{
 
 	render(){
 		return(
 				<div className="intro-wrapper">
 					<div className="intro-name">Hello, I'm Samrat!</div>
-					<div className="tagline">Full Stack Dev | Open-Source Enthusiast | Can't Quit Learning | Ibra</div>
+					<div className="tagline">System Designs | Distributed System | MIcroservices | Books | Football</div>
 					<SocialIcons/>
-					<PlayNine/>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://pdfhost.io/v/3YbsMfG5V_oct26resume"
+						style={{ textDecoration: 'none' }}
+          			>
+					<Button variant="contained" size="large" color="primary">Get CV &nbsp; <CloudDownloadOutlinedIcon fontSize='small'/> </Button>
+					</a>
 				</div>
 
 			)
